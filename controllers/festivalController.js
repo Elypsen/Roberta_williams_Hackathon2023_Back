117505f432bf1a5040e2993abf5805c63aab66c0
Elypsen <bacon.yann@gmail.com>
2023-10-12 13:55:38 +0200

    function name

--- controllers/festivalController.js
+++ controllers/festivalController.js
@@ -1,26 +1,25 @@
-const Festival = require("../models/festival")
+const Festival = require("../models/festival");
 
-
-const getAllFestival = async(req, res) => {
-  try{
-      const festoch = await Festival.find({}).limit(50);
-      res.status(200).json(festoch);
-  }catch(err){
-      console.log("getAllFestival :", err.message)
+const getAllFestival = async (req, res) => {
+  try {
+    const festoch = await Festival.find({}).limit(50);
+    res.status(200).json(festoch);
+  } catch (err) {
+    console.log("getAllFestival :", err.message);
   }
-}
-
-async function getFestivalByDpt(req, res) {
-    try {
-      const departements= await Festival.find({"fields.departement_principal_de_deroulement": req.body.dpt})
-      res.status(200).json(departements);
-    } catch (err) {
-      res.status(404).json({ message: "getDepartements : " + err.message });
-    }
-    
-}
+};
+const getFestivalByDpt = async (req, res) => {
+  try {
+    const departements = await Festival.find({
+      "fields.departement_principal_de_deroulement": req.body.dpt,
+    });
+    res.status(200).json(departements);
+  } catch (err) {
+    res.status(404).json({ message: "getDepartements : " + err.message });
+  }
+};
 
 module.exports = {
   getFestivalByDpt,
   getAllFestival,
-  }
+};
