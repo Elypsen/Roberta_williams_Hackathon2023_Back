746d26301395f6b91bd2fa5f3a6fab866966e76c
unknown <yasmine0927@hotmail.fr>
2023-10-12 14:17:15 +0200

    add getFestivalByNom method

--- controllers/festivalController.js
+++ controllers/festivalController.js
@@ -3,14 +3,13 @@ const Festival = require("../models/festival")
 
 const getAllFestival = async(req, res) => {
   try{
-      const festoch = await Festival.find({}).limit(50);
-      res.status(200).json(festoch);
+      const festivals = await Festival.find({}).limit(50);
+      res.status(200).json(festivals);
   }catch(err){
       console.log("getAllFestival :", err.message)
   }
 }
-
-async function getFestivalByDpt(req, res) {
+const getFestivalByDpt= async(req, res)=> {
     try {
       const departements= await Festival.find({"fields.departement_principal_de_deroulement": req.body.dpt})
       res.status(200).json(departements);
@@ -19,8 +18,22 @@ async function getFestivalByDpt(req, res) {
     }
     
 }
+const getFestivalByNom= async(req, res)=> {
+    try {
+      const partielName = req.body.nom; // Les trois premières lettres du nom du festival
+    const regex = new RegExp(`^${partielName}`, 'i'); // Crée une expression régulière pour correspondre aux trois premières lettres (insensible à la casse)
+      const festivalName= await Festival.find({
+        'fields.nom_du_festival': { $regex: regex }
+      })
+      res.status(200).json(festivalName);
+    } catch (err) {
+      res.status(404).json({ message: "festivalName : " + err.message });
+    }
+    
+}
 
 module.exports = {
   getFestivalByDpt,
+  getFestivalByNom,
   getAllFestival,
   }
--- routes/Router.js
+++ routes/Router.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const {getAllFestival, getFestivalByDpt} = require('../controllers/festivalController')
+const {getAllFestival, getFestivalByDpt, getFestivalByNom} = require('../controllers/festivalController')
 
 router.get('/', getAllFestival);
 router.get('/dpt', getFestivalByDpt)
+router.get('/nom', getFestivalByNom)
 
 module.exports= router;
